4e15dc803959e22ce9b0ad146fe0477475503ed8
Marco Franceschi <mfranceschi@talpor.com>
2021-05-15 19:40:12 -0400

    Filled manifest data

--- gatsby-config.js
+++ gatsby-config.js
@@ -22,8 +22,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Gatsby Simplefolio`,
-        short_name: `Simplefolio`,
+        name: `Marco Franceschi Portfolio`,
+        short_name: `Marco Franceschi`,
         start_url: `/`,
         background_color: `#fff`,
         theme_color: `#02aab0`,
