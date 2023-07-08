8b8d32ec8b2b42ba84ad0d20900582cc2cb12841
Marco Franceschi <marco.franceschi@autocloud.dev>
2023-07-08 09:38:53 -0400

    feat: Created Line Component

---
+++ src/components/Line/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import styles from './line.module.css';
+
+const CLASSNAME: { [position: string]: string } = {
+  top: styles.top,
+  bottom: styles.bottom,
+  right: styles.right,
+  left: styles.left,
+};
+
+const Line = ({ position }: { position: string }) => {
+  return <div className={`${styles.line} ${CLASSNAME[position]}`} />;
+};
+
+export default Line;
