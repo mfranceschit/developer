5ae44e7f3cc33cd6183047122c1349e59dc02f39
Marco Franceschi <marco.franceschi@autocloud.dev>
2023-07-07 21:19:24 -0400

    feat: Clean slate moved from gatsby to next

---
+++ src/pages/about/index.tsx
@@ -0,0 +1,13 @@
+import React, { useContext, useState, useEffect } from 'react';
+
+const About = () => {
+
+
+  return (
+    <section id="about">
+  <h1>About</h1>
+    </section>
+  );
+};
+
+export default About;
