9e635c14052c2ea26b55052d3ba0e31dd3025518
Marco Franceschi <marco.franceschi@autocloud.dev>
2023-07-08 20:08:30 -0400

    feat: Added Title component

--- src/components/Title/index.tsx
+++ src/components/Title/index.tsx
@@ -1,8 +1,8 @@
-import React from 'react';
+import React, { ReactNode } from 'react';
+import styles from './styles.module.css';
 
-
-const Title = ({ children }) => (
-    <h2>{children}</h2>
+const Title = ({ children }: { children: ReactNode }) => (
+  <h1 className={styles.title}>{children}</h1>
 );
 
 export default Title;
