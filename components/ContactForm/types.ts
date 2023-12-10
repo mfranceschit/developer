1cd457eccd29a828645205c90f2c10a372820410
Marco Franceschi <marco.franceschi@autocloud.dev>
2023-12-10 13:52:58 -0300

    feat: Integrated formspree with ContactForm

--- components/ContactForm/types.ts
+++ components/ContactForm/types.ts
@@ -1,15 +1,5 @@
-export interface CustomElements extends HTMLFormControlsCollection {
-  subject: HTMLInputElement;
-  message: HTMLTextAreaElement;
-}
-
-export interface ContactFormPayload extends HTMLFormElement {
-  readonly elements: CustomElements;
-}
-
 export interface ContactFormProps {
   title: string;
-  placeholderSubject: string;
   placeholderMessage: string;
   cta: string;
 }
---
+++ constants/environment.ts
@@ -0,0 +1,1 @@
+export const formspreeKey = process.env.NEXT_PUBLIC_FORMSPREE_KEY || '';
