2efa65bc753aee683a7fff30d982145d67f23b6e
Marco Franceschi <marco.franceschi@autocloud.dev>
2023-12-15 20:25:49 -0300

    feat: Support for sanity cdn images

--- next.config.js
+++ next.config.js
@@ -8,6 +8,15 @@ const nextConfig = {
     includePaths: ['./'],
     prependData: `@import "./styles/theme/theme.scss";`,
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'cdn.sanity.io',
+        port: '',
+      },
+    ],
+  },
 };
 
 module.exports = nextConfig;
