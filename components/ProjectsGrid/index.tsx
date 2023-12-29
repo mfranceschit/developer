580f14913b7c2d346fd85cb9ee6998eaa3ccb74f
Marco Franceschi <marco.franceschi@autocloud.dev>
2023-12-29 10:42:44 -0300

    feat: Removed old details page

--- components/ProjectsGrid/index.tsx
+++ components/ProjectsGrid/index.tsx
@@ -1,71 +1,19 @@
-'use client';
-
-import React, { useState } from 'react';
-import { motion, AnimatePresence } from 'framer-motion';
+import React from 'react';
 
 import ProjectCard from '../ProjectCard';
-import ProjectDetails from '../ProjectDetails';
-import { Project } from '@/types';
+import { Project } from '@/types/Project';
 import styles from './project-grid.module.scss';
 
-const wrapperVariants = {
-  initial: {
-    clipPath: 'polygon(0 0, 0 0, 0 100%, 0% 100%)',
-    transition: { duration: 0.6 },
-  },
-  animate: {
-    clipPath: 'polygon(0 0, 100% 0, 100% 100%, 0 100%)',
-    transition: { duration: 0.6, staggerChildren: 0.1 },
-  },
-  exit: {
-    clipPath: 'polygon(100% 0, 100% 0, 100% 100%, 100% 100%)',
-    transition: { duration: 0.6 },
-  },
-};
-
-export const ProjectsGrid = ({ projects = [] }: { projects: Project[] }) => {
-  const [selectedProject, setSelectedProject] = useState<Project | undefined>();
-
+export const ProjectsGrid: React.FC<{ projects: Project[] }> = ({
+  projects = [],
+}) => {
   return (
     <div className={`${styles.cpTransition} ${styles.cpTransitionContainer}`}>
-      <AnimatePresence mode="sync" initial={false}>
-        {selectedProject ? (
-          <motion.div
-            className={`${styles.card} ${styles.cardWrapper} `}
-            key="card"
-            variants={wrapperVariants}
-            initial="initial"
-            animate="animate"
-            exit="exit">
-            <ProjectDetails
-              project={selectedProject}
-              setSelectedProject={setSelectedProject}
-            />
-          </motion.div>
-        ) : (
-          <motion.div
-            className={styles.cpTransitionSquaresWrapper}
-            key="squares"
-            variants={{
-              ...wrapperVariants,
-              exit: {
-                opacity: 1,
-                transition: { duration: 0.4 },
-              },
-            }}
-            initial="initial"
-            animate="animate"
-            exit="exit">
-            {projects.map((project: Project, i) => (
-              <ProjectCard
-                key={i}
-                project={project}
-                setSelectedProject={setSelectedProject}
-              />
-            ))}
-          </motion.div>
-        )}
-      </AnimatePresence>
+      <div className={styles.cpTransitionSquaresWrapper}>
+        {projects.map(project => (
+          <ProjectCard key={project._id} project={project} />
+        ))}
+      </div>
     </div>
   );
 };
