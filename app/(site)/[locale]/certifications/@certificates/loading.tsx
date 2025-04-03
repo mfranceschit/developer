30eb63847a9ee8607bb6381c8de43df92370a219
Marco Franceschi <franceschi.marco@gmail.com>
2025-04-03 16:54:00 +0200

    fix: Add container for certification loaders

--- app/(site)/[locale]/certifications/@certificates/loading.tsx
+++ app/(site)/[locale]/certifications/@certificates/loading.tsx
@@ -1,19 +1,21 @@
 import ContentLoader from 'react-content-loader';
 
 const Loading: React.FC = props => (
-  <ContentLoader
-    speed={2}
-    width={800}
-    height={230}
-    viewBox="0 0 800 230"
-    backgroundColor="#f3f3f3"
-    foregroundColor="#ecebeb"
-    {...props}>
-    <rect x="-2" y="28" rx="2" ry="2" width="192" height="14" />
-    <rect x="0" y="60" rx="2" ry="2" width="160" height="160" />
-    <rect x="192" y="58" rx="2" ry="2" width="160" height="160" />
-    <rect x="384" y="57" rx="2" ry="2" width="160" height="160" />
-  </ContentLoader>
+  <div>
+    <ContentLoader
+      speed={2}
+      width={800}
+      height={230}
+      viewBox="0 0 800 230"
+      backgroundColor="#f3f3f3"
+      foregroundColor="#ecebeb"
+      {...props}>
+      <rect x="-2" y="28" rx="2" ry="2" width="192" height="14" />
+      <rect x="0" y="60" rx="2" ry="2" width="160" height="160" />
+      <rect x="192" y="58" rx="2" ry="2" width="160" height="160" />
+      <rect x="384" y="57" rx="2" ry="2" width="160" height="160" />
+    </ContentLoader>
+  </div>
 );
 
 export default Loading;
